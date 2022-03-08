bed2da2335c0d353faaacf190a293356fe9ae567
Zane Schaffer <contact@zane.wiki>
2022-03-07 22:57:20 -0800

    added dark mode!

---
+++ darkmode.js
@@ -0,0 +1,10 @@
+var d = new Date();
+var h = d.getHours();
+
+if (h >= 16) {
+  document.body.className = "darkmode";
+  let images = document.getElementsByClassName("blogImgContainer");
+  for (let i = 0; i < images.length; i++) {
+    images[i].classList.add("darkBlogImgContainer");
+  }
+}
